717a396558288a2980b6a81e50b8984ad372cb1c
Paolo Payumo & Tyler Stauss <pair+paopay.tylerstauss@devbootcamp.com>
2014-04-24 16:14:29 -0700

    rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SportsOverload::Application.config.secret_token = '21a9668847ff5a1aa5d99b347a6a23661c1002596de6299a691dde61cc52472efe62a10257dcd5e7d26498636253dba3ea98b071aba89b45711cf9ad6e9e2570'
