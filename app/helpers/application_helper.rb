6e245e7412a9d0ab5aeb9aeaffbe098b01caaba2
Apprentice <apprentice@dbc28.local>
2014-04-25 11:06:32 -0700

    user can log out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
 end
+
+module AuthenticateHelper
+  def current_user
+    if session[:user_id]
+      return User.find(session[:user_id])
+    end
+  end
+end
--- spec/features/log_out_spec.rb
+++ spec/features/log_out_spec.rb
@@ -1,23 +1,22 @@
 require 'spec_helper'
 
 feature "log out" do
-  let!(:user_attr) { FactoryGirl.attributes_for :user }
-  scenario "able to see log out button" do
+  let!(:user) { FactoryGirl.create :user }
+
+  before :each do
     visit root_path
     click_on "Log In"
-    fill_in 'Username', :with => user_attr[:username]
-    fill_in 'Password', :with => user_attr[:password]
+    fill_in 'Username', :with => user.username
+    fill_in 'Password', :with => user.password
     click_on "Sign In"
+  end
+
+  scenario "able to see log out button" do
     expect(page).to have_content("Log Out")
   end
 
-  # scenario "able to log out" do
-  #   visit root_path
-  #   click_on "Log In"
-  #   fill_in 'Username', :with => user_attr[:username]
-  #   fill_in 'Password', :with => user_attr[:password]
-  #   click_on "Sign In"
-  #   click_on "Log Out"
-  #   expect(page).to have_content("Logged Out!")
-  # end
+  scenario "able to log out" do
+    click_on "Log Out"
+    expect(page).to have_content("Log In")
+  end
 end
