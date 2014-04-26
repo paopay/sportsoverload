5ea7c6298c57be7cdc78eb54a48b51c40aabf798
Charles Arciniega & Greg Gerla <pair+arcin.gregorygerla@devbootcamp.com>
2014-04-26 16:54:56 -0700

    broke homepage up in to partials

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -24,7 +24,6 @@ class QuestionsController < ApplicationController
   def show
     @comment = Comment.new
     @answer = Answer.new
-
     @question = Question.find params[:id]
     @answers = @question.order_answers_by_latest
   end
